85d394834f637fe3044ae5cc4f1750667207f713
MANDLESHWAR Pratik <PMR22@dsone.3ds.com>
2023-07-26 10:30:46 +0530

    Flight test

--- InterfaceProject/src/FlightTest.java
+++ InterfaceProject/src/FlightTest.java
@@ -1,42 +1,123 @@
-interface Flying{
-	void flying();
-}
 
-interface Landing{
+public class FlightTest {
+	public static void main(String[] args) {
+		CommercialFlight commFlight = new CommercialFlight();
+		commFlight.checkTicket();
+		commFlight.checkIdentityOfCPassengers();
+		commFlight.fly();
+		commFlight.land();
+		
+		System.out.println("-----------");
+		
+		FighterFlight fightFlight = new FighterFlight();
+		fightFlight.armMissiles();
+		fightFlight.fly();
+		fightFlight.fireMissile();
+		fightFlight.land();
+		
+		System.out.println("-----------");
+		
+		CargoFlight cargoFlight = new CargoFlight();
+		cargoFlight.checkTheCapacity();
+		cargoFlight.carryGoods();
+		cargoFlight.fly();
+		cargoFlight.land();
+		
+		
+		
+	}
+}
+interface Flying {
+	void fly();
+}
+interface Landing {
 	void land();
 }
-
-abstract class Flight implements Flying, Landing{
+abstract class Flight implements Flying,Landing {
 	
 }
 
-interface Ticketing{
+interface Ticketing {
 	void checkTicket();
 }
-class CommercialFlight extends Flight implements Ticketing{
+interface Fighting {
+	void fireMissile();
+}
+interface Carrying {
+	void carryGoods();
+}
+
+class CommercialFlight extends Flight implements Ticketing {
 
 	@Override
-	public void flying() {
-		// TODO Auto-generated method stub
+	public void fly() {
+		System.out.println("Commercial Flight is flying");
 		
 	}
 
 	@Override
 	public void land() {
-		// TODO Auto-generated method stub
+		System.out.println("Commercial Flight is landing");
 		
 	}
 
 	@Override
 	public void checkTicket() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Commercial Flight ...checking tickets....");		
+	}
+	
+	void checkIdentityOfCPassengers() {
+		System.out.println("Commercial Flight is checking identities of passengers...");
 	}
 	
 }
 
-public class FlightTest {
-	public static void main(String[] args) {
+class FighterFlight extends Flight implements Fighting {
+
+	@Override
+	public void fly() {
+		System.out.println("FighterFlight Flight is flying");
 		
 	}
+
+	@Override
+	public void land() {
+		System.out.println("FighterFlight Flight is landing");
+		
+	}
+
+	@Override
+	public void fireMissile() {
+		System.out.println("FighterFlight  ... firing missile....");		
+	}
+	
+	public void armMissiles() {
+		System.out.println("Fighter Flight is arming the missiles...");
+	}
+	
+}
+
+class CargoFlight extends Flight implements Carrying {
+
+	@Override
+	public void fly() {
+		System.out.println("CargoFlight Flight is flying");
+		
+	}
+
+	@Override
+	public void land() {
+		System.out.println("CargoFlight Flight is landing");
+		
+	}
+
+	@Override
+	public void carryGoods() {
+		System.out.println("CargoFlight  ... carrying goods....");		
+	}
+	
+	void checkTheCapacity() {
+		System.out.println("CargoFlight is checking the carriage capacity ");
+	}
+	
 }
