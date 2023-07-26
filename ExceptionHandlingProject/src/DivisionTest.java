1402c9df159e11af8935868f57c6e9a3ebb0f1d4
MANDLESHWAR Pratik <PMR22@dsone.3ds.com>
2023-07-26 12:45:45 +0530

    Throw Throws try catch

--- ExceptionHandlingProject/src/DivisionTest.java
+++ ExceptionHandlingProject/src/DivisionTest.java
@@ -1,5 +1,5 @@
 class Calculator{
-	static void divide(int num, int deno) {
+	static void divide(int num, int deno) throws Exception {
 		System.out.println("------------------------------");
 		System.out.println("Dividing");
 		System.out.println("Numerator " + num);
@@ -12,6 +12,10 @@ class Calculator{
 //		else {
 //			System.out.println("CANNOT DIVIDE BY ZERO");
 //		}
+		if(deno==0)
+		{
+			throw new Exception("The deno is zero");
+		}
 		int divison=num/deno;
 		System.out.println("Divison "+divison);
 		System.out.println("End of program");
@@ -21,14 +25,35 @@ class Calculator{
 public class DivisionTest {
 	 public static void main(String[] args) {
 		System.out.println("Begin");
-		Calculator.divide(100, 4);
 		try {
-			Calculator.divide(40, 0);
+			Calculator.divide(100, 4);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("The exception is "+e);
 		}
-		catch(ArithmeticException ex) {
-			System.out.println(ex);
+		finally {
+			System.out.println("The program is run regardless of the outcome and the inputs");
 		}
-		Calculator.divide(50,5);
+		
+		try {
+			Calculator.divide(100, 0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("The exception is "+e);
+		}
+		finally {
+			System.out.println("The program is run regardless of the outcome and the inputs");
+		}
+		
+		try {
+			Calculator.divide(50,5);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("The exception is "+e);
+		}
+		finally {
+			System.out.println("The program is run regardless of the outcome and the inputs");
+		}
 		System.out.println("End");
 	 }
 }
